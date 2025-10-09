3585cbede2f95e1765a3f57b3c7e34c2d94bc4bd
SOMEONE <77111491+odbxtest@users.noreply.github.com>
2025-10-09 20:28:20 +0330

    Update AWG.sh

--- AWG.sh
+++ AWG.sh
@@ -52,6 +52,8 @@ echo "Hash generated successfully (first 10 chars): ${panelPasswordHash:0:10}...
 echo "Starting AmneziaWG-Easy container..."
 
 # ---------------------------
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 rm -r $ddv_path
 # ---------------------------
 
