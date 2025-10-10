81b1bf90f0e662feccf62653cba6bedcab2027d8
SOMEONE <77111491+odbxtest@users.noreply.github.com>
2025-10-10 06:47:04 +0330

    Update update.sh

--- update.sh
+++ update.sh
@@ -114,8 +114,6 @@ if ! docker compose version &> /dev/null; then
     error "Docker Compose is required. Please ensure it's installed or use Docker's compose plugin."
 fi
 
-mkdir -p "$ddv_path"
-
 hostname -I
 echo ""
 
